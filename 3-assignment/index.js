25fb9069fcb7f4ce69cfb1c7dd3ddfacf201e589
Abdi-Arslene <74920057+Abdi-Arslene@users.noreply.github.com>
2021-03-24 13:47:01 +0100

    last update

--- 3-assignment/index.js
+++ 3-assignment/index.js
@@ -9,7 +9,15 @@ https://api.themoviedb.org/3/person/155/movie_credits?api_key=9bc3828de117c1e056
 
 addEventListener('load', function() {
     call_Movie('joker')
-})
+});
+ addEventListener("keydown", function(pressed_key) {
+  pressed_key=pressed_key.keyCode;
+  if (pressed_key === 13) {
+   let but=document.getElementsByClassName("btn btn-primary");
+   but[but.length-1].click();
+  }
+});
+
 let tab=["joker"];
 function call_Movie(search){
     let url = 'https://api.themoviedb.org/3/search/movie?api_key=9bc3828de117c1e056c726e3f56dcac6&query=' + search
@@ -19,7 +27,7 @@ function call_Movie(search){
         console.log(data.results[0])
         insertIntoHTML(data)
     }).catch(function(error){
-        console.log('Something went wrong')
+        console.log('There is a problem: Check it out')
         console.log(error)
     })
 }
@@ -33,6 +41,9 @@ id_film=elem.id;
   console.log(id_film);
 
 }
+
+
+
 
 function response(){
 
@@ -64,6 +75,7 @@ function call_cast(data,actor){
         return res.json()
     }).then(function(data){
         console.log(data.cast[0].id);
+        
         compare_cast(data,actor)
     }).catch(function(error){
         console.log('There is a problem: Check it out')
@@ -73,7 +85,7 @@ function call_cast(data,actor){
 
 function compare_cast(data,actor){
   let i=0;
-  let exist=0;
+  let exist = 0;
   let casts=data.cast;
   let baseUrl = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
   while(i<casts.length){
@@ -81,7 +93,9 @@ function compare_cast(data,actor){
       exist=1;
       buttons=document.getElementsByClassName("btn btn-primary")
       buttons[buttons.length-1].disabled=true;
-      document.body.innerHTML += '<div class="list"> <h1><a>' + actor + ' </a></h1> <img src=' +baseUrl +casts[i].profile_path+' class="img-fluid rounded d-block img-thumbnail"></div><div class="form"> <div class="form-group"> <input type="text" class="form-control" onfocus="reset(this)" placeholder="A Movie where he played"></div><button class="btn btn-primary" Onclick="response2(\'' + casts[i].id + '\')">Submit</button></div>';
+      respons=document.getElementsByClassName("form-control");
+    respons[respons.length-1].disabled=true;
+      document.body.innerHTML += '<div class="list"> <h1><a>' + actor + ' </a></h1> <img src=' +baseUrl +casts[i].profile_path+' class="img-fluid rounded d-block img-thumbnail"></div><div class="form"> <div class="form-group"> <input type="text" class="form-control" onfocus="reset(this)" placeholder="A Movie where he played"></div><button class="btn btn-primary" Onclick="response2(\''+ casts[i].id +'\')">Submit</button></div>';
       break;
     }
     i++;
@@ -108,13 +122,15 @@ function response2(id_actor){
        res[res.length-1].style.color="red";
     }
     else{
-      tab.push(res[res.length-1].value);
-      call_films_id(id_actor,res[res.length-1].value);
+        
+        call_films_id(id_actor,res[res.length-1].value,tab);
+        console.log("djj  "+ id_actor);
+      
     }
     
 }
 
-function call_films_id(id_actor,rep){
+function call_films_id(id_actor,rep,tab){
   console.log(id_actor);
 
   let url='https://api.themoviedb.org/3/person/'+ id_actor +'/movie_credits?api_key=9bc3828de117c1e056c726e3f56dcac6&language=en-US'
@@ -122,14 +138,14 @@ function call_films_id(id_actor,rep){
         return res.json()
     }).then(function(data){
         console.log(rep);
-        compare_movie(data,rep)
+        compare_movie(data,rep,tab)
     }).catch(function(error){
         console.log('There is a problem: Check it out')
         console.log(error)
     })
 }
 
-function compare_movie(data,movie){
+function compare_movie(data,movie,tab){
   let i=0;
   let exist=0;
   let casts=data.cast;
@@ -137,8 +153,11 @@ function compare_movie(data,movie){
   while(i<casts.length){
     if(movie.toLowerCase()==casts[i].original_title.toLowerCase()){
       exist=1;
+      tab.push(movie);
       buttons=document.getElementsByClassName("btn btn-primary")
       buttons[buttons.length-1].disabled=true;
+ respons=document.getElementsByClassName("form-control");
+    respons[respons.length-1].disabled=true;
       document.body.innerHTML += '<div class="list"> <h1><a>' + movie + ' </a></h1> <img src=' +baseUrl +casts[i].poster_path+' class="img-fluid rounded d-block img-thumbnail"></div><div class="form"> <div class="form-group"> <input type="text" class="form-control" onfocus="reset(this)" placeholder="Enter the director or an actor of the movie"></div><button class="btn btn-primary" Onclick="response()">Submit</button></div>';
       break;
     }
